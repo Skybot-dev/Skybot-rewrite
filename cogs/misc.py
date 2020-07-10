7fbe0e7971211285b95918507023583ab304f40f
Patrick Jones <patrickjones@live.co.uk>
2020-07-10 13:56:10 +0100

    pressed space a few times

--- cogs/misc.py
+++ cogs/misc.py
@@ -15,9 +15,8 @@ from trello import TrelloClient
 class Misc(commands.Cog, name="Misc"):
     """Miscellaneous commands"""
     def __init__(self, bot):
-        self.bot = bot
-        self.client = TrelloClient(api_key=get_config()["trello"]["api_key"], api_secret=get_config()["trello"]["api_secret"])
-        self.my_board = self.client.get_board(get_config()["trello"]["board_id"])
+        self.bot = bot 
+        self.my_board = self.bot.trelloBoard
         self.my_lists = self.my_board.list_lists()
         self.stats.start()
     
@@ -27,9 +26,12 @@ class Misc(commands.Cog, name="Misc"):
         embed.add_field(name="--------------------------------", value=f"The bot already is on {len(self.bot.guilds)} Guilds!\n[Click Here to invite the Bot]({get_config()['bot_invite']})")
         await embed.set_made_with_love_footer()
         await ctx.send(embed=embed)
+
+        
     @commands.group(invoke_without_command=True, name="suggestion", description="Handles suggestions for the bot", aliases=["suggest"], usage="[submit/view]")
     async def suggestion(self, ctx, arg=None):
         await ctx.invoke(self.bot.get_command("help show_command"), arg="suggestion")
+    
     @suggestion.command()
     async def submit(self, ctx, *, answer:str):
         embed = Embed(description=answer, bot=self.bot, user=ctx.author).set_author(name=ctx.author, icon_url=ctx.author.avatar_url)
@@ -43,6 +45,7 @@ class Misc(commands.Cog, name="Misc"):
         card = self.my_lists[0].add_card(answer)
         suggestion_doc = await self.bot.admin_db["suggestions"].insert_one({"status": "submitted", "user": ctx.author.id, "content": answer, "message": msg.id, "datetime": time.time(), "card": card.id})
         await msg.edit(embed=msg.embeds[0].set_footer(text=f"ID: {str(suggestion_doc.inserted_id)}"))
+    
     async def view_suggestion(self, ctx, id:str):
         suggestion_doc = await self.bot.admin_db["suggestions"].find_one({"_id": ObjectId(id)})
         if not suggestion_doc:
@@ -57,9 +60,11 @@ class Misc(commands.Cog, name="Misc"):
         embed.add_field(name="More", value=f"[trello page](https://trello.com/b/2yBAtx82/skybot-rewrite) | [GitHub repository](https://github.com/Skybot-dev/Skybot-rewrite)", inline=False)
         embed.timestamp = datetime.fromtimestamp(suggestion_doc["datetime"])
         return embed
+    
     @suggestion.command()
     async def view(self, ctx, id:str):
         await ctx.send(embed=await self.view_suggestion(ctx, id))
+    
     @suggestion.command()
     @commands.check(is_staff)
     async def list(self, ctx):
@@ -69,6 +74,7 @@ class Misc(commands.Cog, name="Misc"):
         msg = await ctx.send(embed=embeds[0])
         pages = Paginator(self.bot, msg, embeds=embeds, timeout=60, use_more=True, only=ctx.author)
         await pages.start()
+    
     @suggestion.command()
     @commands.check(is_staff)
     async def move(self, ctx, id:str, *, section:str):
@@ -82,11 +88,13 @@ class Misc(commands.Cog, name="Misc"):
         card.change_list(List[0].id)
         await self.bot.admin_db["suggestions"].update_one({"_id": ObjectId(id)}, {"$set": {"status": section}})
         await ctx.send(f"Moved card `{card.name}` to List `{List[0].name}`")
+    
     @suggestion.command()
     @commands.check(is_staff)
     async def clear(self, ctx):
         await self.bot.admin_db["suggestions"].delete_many({})
         await ctx.send("cleard all suggestions")
+    
     @suggestion.command()
     @commands.check(is_staff)
     async def delete(self, ctx, id:str):
@@ -96,19 +104,26 @@ class Misc(commands.Cog, name="Misc"):
         card =  self.client.get_card(suggestion_doc["card"])
         card.delete()
         await ctx.send(f"successfully deleted suggestion `{id}`")
+    
+    
     @commands.group(invoke_without_command=True, name="changelog", description="bot changelogs", aliases=["update", "updates", "changelogs"])
     async def changelog(self, ctx):
         await ctx.invoke(self.bot.get_command("help show_command"), arg="changelog")
+    
     async def changelog_embed(self, ctx, json_doc):
         embed = Embed(title=f"Changelogs for {json_doc['type']} `{json_doc['version']}`", description=json_doc["description"], bot=self.bot, user=ctx.author)
         embed.add_field(name="Bugs/plans", value=json_doc["bugs"], inline=False)
         embed.timestamp = datetime.fromtimestamp(json_doc["date"])
         await embed.set_made_with_love_footer()
         return embed
+    
     @changelog.command(name="latest")
     async def latest(self, ctx):
         update = await self.bot.admin_db["changelog"].find_one({"latest": True})
+        if not update:
+            return await ctx.send("could not find any changelogs")
         await ctx.send(embed=(await self.changelog_embed(ctx, update)))
+    
     @changelog.command()
     @commands.check(is_staff)
     async def add(self, ctx, Type, version, description, bugs):
@@ -116,6 +131,7 @@ class Misc(commands.Cog, name="Misc"):
         json_data = {"type": Type, "version": version, "bugs": bugs, "description": description, "date": time.time(), "latest": True}
         await self.bot.admin_db["changelog"].insert_one(json_data)
         await ctx.send(embed=(await self.changelog_embed(ctx, json_data)))
+    
     @changelog.command()
     async def version(self, ctx, v:str):
         v = v.replace('v', '')
@@ -123,11 +139,13 @@ class Misc(commands.Cog, name="Misc"):
         if not update:
             return await ctx.send("Could not find that version")
         await ctx.send(embed=(await self.changelog_embed(ctx, update)))
+    
     @changelog.command()
     @commands.check(is_staff)
     async def clear(self, ctx):
         await self.bot.admin_db["changelog"].delete_many({})
         await ctx.send("deleted all changelogs")
+    
     @changelog.command()
     async def list(self, ctx):
         Embeds = []
@@ -139,10 +157,16 @@ class Misc(commands.Cog, name="Misc"):
             #print(Embeds[len(Embeds) - 1])
             Embeds[len(Embeds) - 1].add_field(name=f"{change['type']} {change['version']}", value=datetime.fromtimestamp(change['date']).strftime("%m/%d/%y"))
             count += 1
-        msg = await ctx.send(embed=Embeds[0])
-        if count > 0:
-            pages = Paginator(self.bot, msg, embeds=Embeds, timeout=60, use_more=True, only=ctx.author)
-            await pages.start()
+        print(count)
+        if not count == 0:
+            msg = await ctx.send(embed=Embeds[0])
+            if count > 0:
+                pages = Paginator(self.bot, msg, embeds=Embeds, timeout=60, use_more=True, only=ctx.author)
+                await pages.start()
+        else:
+            return await ctx.send("Could not find any changelogs")
+    
+    
     @commands.Cog.listener()
     async def on_guild_join(self, guild):
         config = get_config()
@@ -151,6 +175,7 @@ class Misc(commands.Cog, name="Misc"):
         #msg = await logchannel.fetch_message(config["support_guild"]["stats"]["message"])
         embed = discord.Embed(title="Guild add", description=f"NAME: {guild.name} \nID: {guild.id} \nMembers: {len(guild.members)}", color=0x00FF00)
         await logchannel.send(embed=embed)
+    
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         config = get_config()
@@ -158,6 +183,8 @@ class Misc(commands.Cog, name="Misc"):
         logchannel = logguild.get_channel(config["support_guild"]["log_channel"])
         embed = discord.Embed(title="Guild remove", description=f"NAME: {guild.name} \nID: {guild.id} \nMembers: {len(guild.members)}", color=0xff0000)
         await logchannel.send(embed=embed)
+    
+    
     @tasks.loop(minutes=5)
     async def stats(self):
         config = get_config()["support_guild"]
@@ -184,8 +211,11 @@ class Misc(commands.Cog, name="Misc"):
         embed.add_field(name="Top 10 Member count:", value=final_list, inline=False)
         await embed.set_made_with_love_footer()
         await message.edit(embed=embed)
+    
     @stats.before_loop
     async def before_stats(self):
         await self.bot.wait_until_ready()
+
+
 def setup(bot):
     bot.add_cog(Misc(bot))
