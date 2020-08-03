08f186c8c6b219ffa5003750fc9014d39a688d04
lolieg <52113953+lolieg@users.noreply.github.com>
2020-08-03 22:48:05 +0200

    command not found msg deactivated

--- bot.py
+++ bot.py
@@ -91,7 +91,7 @@ class Skybot(commands.AutoShardedBot):
 
     async def on_command_error(self, ctx : commands.Context, exception):
         if isinstance(exception, commands.CommandNotFound):
-            return await ctx.send("`Command not found`", delete_after=3)
+            return #await ctx.send("`Command not found`", delete_after=3)
         if isinstance(exception, commands.NoPrivateMessage):
             return await ctx.send("This command can't be used in a private chat.", delete_after=7)
         if isinstance(exception, commands.CommandOnCooldown):
